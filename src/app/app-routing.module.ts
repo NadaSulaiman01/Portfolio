1e4c22379092ca764f83a1e999250a63520f26e8
Nada Sulaiman <nadasulaiman01@gmail.com>
2024-04-24 19:10:14 +0200

    Add fragment scrolling in homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
+  {path : "", redirectTo: "/home", pathMatch:'full'},
+  {path: "home", component:HomePageComponent},
   {path: "projects", loadChildren: () => import('src/app/components/project-details/project-details.module').then(m => m.ProjectDetailsModule)},
-  {path: "**", component: HomePageComponent }
+  //{path: "**", component: HomePageComponent }
 ];
 
 @NgModule({
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+
+  constructor(private activatedRoute : ActivatedRoute, private router : Router) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe((data) =>{
+      this.scrollToSection(data);
+      }
+    );
+   
+  }
+
+  scrollToSection(section : any){
+    const element = document.getElementById(section);
+  if (element) {
+    const rect = element.getBoundingClientRect();
+    let offset = 80;
+    if(section == "home"){
+      offset = 200;
+    }
+    const y = rect.top + window.pageYOffset - offset;
+    window.scrollTo({top: y, behavior: 'smooth'});
+    this.router.navigate([]);
+  }
+  }
 
 }
